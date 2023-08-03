e6c034025c68b0273677979e8cec0f93d0ed15d5
Scala Steward <scala_steward@virtuslab.com>
2023-08-03 14:01:06 +0000

    Update sbt-depts-djx314 to 0.0.1-M728

--- inner-project/depts-codegen/src/main/codegen/djx/sbt/depts/codegen/LibraryPluginDeptsInstance.scala
+++ inner-project/depts-codegen/src/main/codegen/djx/sbt/depts/codegen/LibraryPluginDeptsInstance.scala
@@ -53,7 +53,7 @@ object LibraryPluginDeptsInstance extends _root_.djx.sbt.depts.abs.PluginDepts {
 
   VarContext.changePluginVar := "sbt-depts-djx314"
 
-  addSbtPlugin("net.scalax.djx314" % "sbt-depts-djx314" % "0.0.1-M726")
+  addSbtPlugin("net.scalax.djx314" % "sbt-depts-djx314" % "0.0.1-M728")
 
   VarContext.changePluginVar := "sbt-js-engine"
 
