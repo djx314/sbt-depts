0d1d7f28c9df6a69d152af53e1297b424edf4394
djx314 <djx314@sina.cn>
2024-12-03 04:37:33 +0800

    torchlight

--- inner-project/depts-codegen/src/main/codegen/djx/sbt/depts/codegen/SbtVersionInfo.scala
+++ inner-project/depts-codegen/src/main/codegen/djx/sbt/depts/codegen/SbtVersionInfo.scala
@@ -1,8 +1,10 @@
+
 package djx.sbt.depts
 package codegen
 
 object SbtVersionInfo extends _root_.djx.sbt.depts.abs.TakeSbtProperties {
 
-  override val sbtPropertiesString: String = """sbt.version=1.10.5"""
+  override val sbtPropertiesString: String = """sbt.version=1.10.6"""
 
 }
+
