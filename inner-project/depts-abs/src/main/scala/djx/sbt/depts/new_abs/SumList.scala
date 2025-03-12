75ca6aa9a46b28ba57c97efe5efc0ef9b92e626e
djx314 <djx314@sina.cn>
2025-03-13 01:56:16 +0800

    pythagoras.

--- inner-project/depts-abs/src/main/scala/djx/sbt/depts/new_abs/SumList.scala
+++ inner-project/depts-abs/src/main/scala/djx/sbt/depts/new_abs/SumList.scala
@@ -17,43 +17,45 @@ object SumList {
 
   type FoldType = (ScalaJavaVersion.Type, ChangeModuleIdName, SumModule)
 
+  private def foldActionImpl1(m: FoldType, sv: ScalaJavaVersion.Type): FoldType = {
+    val addToSVOpt: Option[SumType] = sv.fold
+      .apply { (s212: ScalaVersion212) =>
+        Some(sumSetter(s212))
+      }
+      .apply { (s213: ScalaVersion213) =>
+        Some(sumSetter(s213))
+      }
+      .apply { (s3: ScalaVersion3) =>
+        Some(sumSetter(s3))
+      }
+      .apply { (_: JavaVersionForAllScala) =>
+        None
+      }
+
+    val provideList: List[SumType] = m._3.scalaVersions
+    val newList1                   = for (a <- addToSVOpt) yield a :: provideList
+    val newList2                   = newList1.getOrElse(provideList)
+
+    (sv, m._2, m._3.copy(scalaVersions = newList2))
+  }
+
+  private def foldActionImpl2(m: FoldType, deptModule: DeptsModule): FoldType = {
+    val map1: Map[ChangeModuleIdName, List[DeptsWithVersionModel]]   = m._3.depts
+    val value1: List[DeptsWithVersionModel]                          = map1.getOrElse(m._2, List.empty)
+    val deptToAdd: DeptsWithVersionModel                             = deptModule.toScalaJavaVersion(m._1)
+    val newMap: Map[ChangeModuleIdName, List[DeptsWithVersionModel]] = map1 + ((m._2, deptToAdd :: value1))
+    (m._1, m._2, m._3.copy(depts = newMap))
+  }
+
+  private def foldActionImpl3(m: FoldType, cName: ChangeModuleIdName): FoldType =
+    (m._1, cName, m._3.copy(deptNames = cName :: m._3.deptNames))
+
+  private def foldAction(m: FoldType, listItem: SettingInstance.Type): FoldType =
+    listItem.fold(sv => foldActionImpl1(m, sv))(deptModule => foldActionImpl2(m, deptModule))(cName => foldActionImpl3(m, cName))
+
   def sumList(l: List[SettingInstance.Type]): SumModule = {
     val initModel: SumModule = SumModule(List.empty, List.empty, Map.empty)
     val initZero: FoldType   = (null, null, initModel)
-
-    def foldAction(m: FoldType, listItem: SettingInstance.Type): FoldType = listItem.fold
-      .apply { (sv: ScalaJavaVersion.Type) =>
-        {
-          val addToSVOpt: Option[SumType] = sv.fold
-            .apply { (s212: ScalaVersion212) =>
-              Some(sumSetter(s212))
-            }
-            .apply { (s213: ScalaVersion213) =>
-              Some(sumSetter(s213))
-            }
-            .apply { (s3: ScalaVersion3) =>
-              Some(sumSetter(s3))
-            }
-            .apply { (s3: JavaVersionForAllScala) =>
-              None
-            }
-
-          val newList1 = for (a <- addToSVOpt) yield a :: m._3.scalaVersions
-          val newList2 = newList1.getOrElse(m._3.scalaVersions)
-
-          (sv, m._2, m._3.copy(scalaVersions = newList2))
-        }
-      }
-      .apply { (deptModule: DeptsModule) =>
-        val map1: Map[ChangeModuleIdName, List[DeptsWithVersionModel]]   = m._3.depts
-        val value1: List[DeptsWithVersionModel]                          = map1.getOrElse(m._2, List.empty)
-        val deptToAdd: DeptsWithVersionModel                             = deptModule.toScalaJavaVersion(m._1)
-        val newMap: Map[ChangeModuleIdName, List[DeptsWithVersionModel]] = map1 + ((m._2, deptToAdd :: value1))
-        (m._1, m._2, m._3.copy(depts = newMap))
-      }
-      .apply { (cName: ChangeModuleIdName) =>
-        (m._1, cName, m._3.copy(deptNames = cName :: m._3.deptNames))
-      }
 
     val (_, _, foldValue) = l.foldLeft(initZero)(foldAction)
 
