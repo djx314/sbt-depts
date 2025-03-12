9ca3d4b73ecfd24a59847a02eff7c25bb437e37d
djx314 <djx314@sina.cn>
2025-03-12 10:16:03 +0800

    p

--- inner-project/depts-abs/src/main/scala/djx/sbt/depts/new_abs/AddJavaDeptsAbs.scala
+++ inner-project/depts-abs/src/main/scala/djx/sbt/depts/new_abs/AddJavaDeptsAbs.scala
@@ -53,40 +53,68 @@ case class DeptsWithVersionModel(
 )
 
 // ===
+case class ChangeModuleIdName(name: String)
+
+// ===
+trait SettingInstance {
+  protected var protectedSettings: List[SettingInstance.Type]
+  private val setter = Adt.CoProduct3[ScalaJavaVersion.Type, DeptsModule, ChangeModuleIdName]
+  private def addToSettingImpl(s: SettingInstance.Type): SettingInstance.AddedToSetting = {
+    protectedSettings = s :: protectedSettings
+    SettingInstance.AddedToSetting(s)
+  }
+  def outputSettings: List[SettingInstance.Type] = protectedSettings.reverse
+
+  def addLib(t: DeptsModule): SettingInstance.AddedToSetting = {
+    val sModel: SettingInstance.Type = setter(t)
+    addToSettingImpl(sModel)
+  }
+
+  def addScalaVersion(t: ScalaJavaVersion.Type): SettingInstance.AddedToSetting = {
+    val sModel: SettingInstance.Type = setter(t)
+    addToSettingImpl(sModel)
+  }
+
+  def changeModuleName(name: String): SettingInstance.AddedToSetting = {
+    val sModel: SettingInstance.Type = setter(ChangeModuleIdName(name))
+    addToSettingImpl(sModel)
+  }
+}
+
+object SettingInstance {
+  SettingInstanceSelf =>
+
+  case class AddedToSetting(v: SettingInstanceSelf.Type)
+
+  type Type = Adt.CoProduct3[ScalaJavaVersion.Type, DeptsModule, ChangeModuleIdName]
+  def instance(initSettings: List[SettingInstance.Type]): SettingInstance = new SettingInstance {
+    override protected var protectedSettings: List[SettingInstance.Type] = initSettings
+  }
+}
+
+// ===
 trait AddJavaDeptsAbs {
 
-  type SettingType = Adt.CoProduct2[ScalaJavaVersion.Type, DeptsModule]
-  case class AddedToSetting(v: SettingType)
-
-  trait SettingInstance {
-    protected var settings: List[SettingType]
-    private val setter = Adt.CoProduct2[ScalaJavaVersion.Type, DeptsModule]
-
-    def addLib(t: DeptsModule): AddedToSetting = {
-      val sMolde: SettingType = setter(t)
-      settings = sMolde :: settings
-      AddedToSetting(sMolde)
-    }
-    def addScalaVersion(t: ScalaJavaVersion.Type): AddedToSetting = {
-      val sMolde: SettingType = setter(t)
-      settings = sMolde :: settings
-      AddedToSetting(sMolde)
-    }
-  }
-
-  val settingInstance: SettingInstance = new SettingInstance {
-    override protected var settings: List[SettingType] = List.empty
-  }
+  val settingInstance: SettingInstance = SettingInstance.instance(List.empty)
 
   object CrossVersion {
     val full: CrossInfo.Type = CrossInfo.`CrossVersion.full`
   }
 
   object scalaVersion {
-    def :=(str: String): AddedToSetting = sbt.librarymanagement.CrossVersion.partialVersion(str) match {
+    def :=(str: String): SettingInstance.AddedToSetting = sbt.librarymanagement.CrossVersion.partialVersion(str) match {
       case Some((2L, 12L)) => settingInstance.addScalaVersion(ScalaJavaVersion.ScalaVersion212(str))
       case Some((2L, 13L)) => settingInstance.addScalaVersion(ScalaJavaVersion.ScalaVersion213(str))
       case Some((3L, _))   => settingInstance.addScalaVersion(ScalaJavaVersion.ScalaVersion3(str))
+    }
+  }
+
+  object JavaVersionToSetting {
+    object value
+  }
+  object customJV {
+    def :=(str: JavaVersionToSetting.value.type): SettingInstance.AddedToSetting = {
+      settingInstance.addScalaVersion(ScalaJavaVersion.JavaVersion)
     }
   }
 
@@ -96,6 +124,16 @@ trait AddJavaDeptsAbs {
     def %(name: String): DeptsModuleHelper1   = new DeptsModuleHelper1(org = extraSelf.org, name = name, platform = DeptType.JavaDept)
     def %%(name: String): DeptsModuleHelper1  = new DeptsModuleHelper1(org = extraSelf.org, name = name, platform = DeptType.ScalaDept)
     def %%%(name: String): DeptsModuleHelper1 = new DeptsModuleHelper1(org = extraSelf.org, name = name, platform = DeptType.ScalaJSDept)
+  }
+
+  object libraryDependencies {
+    def +=(dept: DeptsModule): SettingInstance.AddedToSetting = settingInstance.addLib(dept)
+  }
+
+  object VarContext {
+    object changeDeptVar {
+      def :=(str: String): SettingInstance.AddedToSetting = settingInstance.changeModuleName(str)
+    }
   }
 
 }
